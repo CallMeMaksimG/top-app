c72116c6e790e7e29e04e7ac0fbadaffe309bc0c
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-05 19:30:24 +0300

    Добавить css переменные

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    // crossOrigin
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&family=Noto+Sans:wght@300;400;500;700&display=swap"
+                    rel="stylesheet"
+                ></link>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+}
