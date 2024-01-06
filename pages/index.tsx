896cf0b1ee8b35975d4120159b86f23476969872
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-06 18:24:11 +0300

    Добавить layout Header, Footer, Sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import { Button, Htag, P, Rating, Tag } from '@/components';
+import { Layout } from '@/layout/Layout';
 import { useState } from 'react';
 
 export default function Page() {
     const [rating, setRating] = useState<number>(4);
     return (
-        <>
+        <Layout>
             <Htag tag="h1">Text</Htag>
             <Button appearance="primary" arrow="right">
                 Button
@@ -14,12 +15,20 @@ export default function Page() {
             </Button>
             <P>Text M</P>
             <P size="s">Text S</P>
-            <P size='l'>Text Large</P>
-            <Tag size='s' color='ghost'>small</Tag>
-            <Tag size='m' color='primary'>medium</Tag>
-            <Tag size='m' color='grey'>med</Tag>
-            <Tag size='m' color='red'>HH</Tag>
+            <P size="l">Text Large</P>
+            <Tag size="s" color="ghost">
+                small
+            </Tag>
+            <Tag size="m" color="primary">
+                medium
+            </Tag>
+            <Tag size="m" color="grey">
+                med
+            </Tag>
+            <Tag size="m" color="red">
+                HH
+            </Tag>
             <Rating rating={rating} isEditable setRating={setRating}></Rating>
-        </>
+        </Layout>
     );
 }
