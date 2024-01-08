56dc02755299e56116733de42b71360a862d4b25
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-08 19:05:22 +0300

    Добавить получение категорий курсов с api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import { Button, Htag, P, Rating, Tag } from '@/components';
 import { withLayout } from '@/layout/Layout';
+import { GetStaticProps } from 'next';
 import { useState } from 'react';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Page() {
+function Page({ menu }: HomeProps): JSX.Element {
+    console.log(menu)
     const [rating, setRating] = useState<number>(4);
     return (
         <>
@@ -29,8 +33,36 @@ function Page() {
                 HH
             </Tag>
             <Rating rating={rating} isEditable setRating={setRating}></Rating>
+            <ul>
+                {menu.map((m) => (
+                    <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+                ))}
+            </ul>
         </>
     );
 }
 
 export default withLayout(Page);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0;
+    const { data: menu } = await axios.post<MenuItem[]>(
+        process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+        {
+            firstCategory,
+        }
+    );
+
+
+    return {
+        props: {
+            menu,
+            firstCategory,
+        },
+    };
+};
+
+interface HomeProps extends Record<string, unknown> {
+    menu: MenuItem[];
+    firstCategory: number;
+}
