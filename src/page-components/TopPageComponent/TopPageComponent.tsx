0e2195ebde6828b32ced926d319d430837636f13
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-13 12:38:41 +0300

    Форматирование seo-text

--- src/page-components/TopPageComponent/TopPageComponent.tsx
+++ src/page-components/TopPageComponent/TopPageComponent.tsx
@@ -46,7 +46,7 @@ export const TopPageComponent = ({
                     <Advantages advantages={page.advantages} />
                 </>
             )}
-            {page.seoText && <P>{page.seoText}</P>}
+            {page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText}}></div>}
             <Htag tag="h2">Получаемые навыки</Htag>
             {page.tags.map(t => <Tag color='primary' key={t}>{t}</Tag>)}
         </div>
