21c9007f6269d48ed815e0055b5ad2b9be73d2d3
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-17 20:52:58 +0300

    Добавить обработку ошибок при отправке формы на компоненте Rating

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -12,7 +12,7 @@ import { RatingProps } from './Rating.props';
 
 export const Rating = forwardRef(
     (
-        { isEditable = false, rating, setRating, ...props }: RatingProps,
+        {error, isEditable = false, rating, setRating, ...props }: RatingProps,
         ref: ForwardedRef<HTMLDivElement>
     ): JSX.Element => {
         const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
@@ -68,10 +68,17 @@ export const Rating = forwardRef(
         };
 
         return (
-            <div {...props} ref={ref}>
+            <div
+                {...props}
+                ref={ref}
+                className={cn(styles.ratingWrapper, {
+                    [styles.error]: error
+                })}
+            >
                 {ratingArray.map((r, i) => (
                     <span key={i}>{r}</span>
                 ))}
+                {error && <span className={styles.errorMessage}>{error.message}</span>}
             </div>
         );
     }
