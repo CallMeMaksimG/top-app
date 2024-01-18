95a0e9fbd5512dd638443870b2827e140edc2aad
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-18 13:25:35 +0300

    Рефакторинг (вынести пути в helpers api)

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { GetStaticProps } from 'next';
 import { useState } from 'react';
 import axios from 'axios';
 import { MenuItem } from '../interfaces/menu.interface';
+import { API } from '../helpers/api';
 
 function Page({ menu }: HomeProps): JSX.Element {
     const [rating, setRating] = useState<number>(4);
@@ -42,12 +43,9 @@ export default withLayout(Page);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
     const firstCategory = 0;
-    const { data: menu } = await axios.post<MenuItem[]>(
-        process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
-        {
-            firstCategory,
-        }
-    );
+    const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+        firstCategory,
+    });
 
     return {
         props: {
