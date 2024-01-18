f5d575d37a02d7384c4758f71cac8cdb34bb0d25
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-18 13:46:12 +0300

    Добавить отправку формы отзыва

--- src/components/ReviewForm/ReviewForm.tsx
+++ src/components/ReviewForm/ReviewForm.tsx
@@ -4,7 +4,10 @@ import { Controller, useForm } from 'react-hook-form';
 import styles from './ReviewForm.module.scss';
 import { Button, Input, Rating, Textarea } from '..';
 import CloseIcon from './close.svg';
-import { IReviewForm } from './ReviewForm.interface';
+import { IReviewForm, IReviewSentResponse } from './ReviewForm.interface';
+import axios from 'axios';
+import { API } from '../../../helpers/api';
+import { useState } from 'react';
 
 export const ReviewForm = ({
     productId,
@@ -16,9 +19,25 @@ export const ReviewForm = ({
         control,
         handleSubmit,
         formState: { errors },
+        reset,
     } = useForm<IReviewForm>();
-    const onSubmit = (data: IReviewForm) => {
-        console.log(data);
+    const [isSuccess, setIsSuccess] = useState<boolean>(false);
+    const [error, setError] = useState<string>();
+    const onSubmit = async (formData: IReviewForm) => {
+        try {
+            const { data } = await axios.post<IReviewSentResponse>(
+                API.review.createDemo,
+                { ...formData, productId }
+            );
+            if (data.message) {
+                setIsSuccess(true);
+                reset();
+            } else {
+                setError('Что-то пошло не так');
+            }
+        } catch (e) {
+            setError(e.message);
+        }
     };
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -46,7 +65,12 @@ export const ReviewForm = ({
                     <Controller
                         control={control}
                         name="rating"
-                        rules={{required: { value: true, message: 'Укажите рейтинг' }}}
+                        rules={{
+                            required: {
+                                value: true,
+                                message: 'Укажите рейтинг',
+                            },
+                        }}
                         render={({ field }) => (
                             <Rating
                                 isEditable
@@ -77,11 +101,23 @@ export const ReviewForm = ({
                     </span>
                 </div>
             </div>
-            <div className={styles.success}>
-                <div className={styles.successTitle}>Ваш отзыв отправлен</div>
-                <div>Спасибо, ваш отзыв будет опубликован после проверки</div>
-                <CloseIcon className={styles.close} />
-            </div>
+            {isSuccess && (
+                <div className={cn(styles.success, styles.panel)}>
+                    <div className={styles.successTitle}>
+                        Ваш отзыв отправлен
+                    </div>
+                    <div>
+                        Спасибо, ваш отзыв будет опубликован после проверки
+                    </div>
+                    <CloseIcon className={styles.close} onClick={() => setIsSuccess(false)}/>
+                </div>
+            )}
+            {error && (
+                <div className={cn(styles.error, styles.panel)}>
+                    Что-то пошло не так, попробуйте обновить страницу.
+                    <CloseIcon className={styles.close} onClick={() => setError(undefined)}/>
+                </div>
+            )}
         </form>
     );
 };
