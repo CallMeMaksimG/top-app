79add2dd7965c38d96b899790a94e0b023f775f9
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-19 23:09:20 +0300

    Добавить мобильное меню

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,6 +1,55 @@
 import { HeaderProps } from './Header.props';
-// import styles from './Header.module.scss';
+import styles from './Header.module.scss';
+import Logo from '../logo.svg';
+import cn from 'classnames';
+import { ButtonIcon } from '@/components/ButtonIcon/ButtonIcon';
+import { motion } from 'framer-motion';
+import { Sidebar } from '../Sidebar/Sidebar';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
-export const Header = ({ ...props }: HeaderProps): JSX.Element => {
-    return <div {...props}>Header</div>;
+export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
+    const [isOpend, setIsOpend] = useState<boolean>(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        setIsOpend(false);
+    }, [router]);
+    const variants = {
+        opened: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                stiffness: 20,
+            },
+        },
+        closed: {
+            opacity: 0,
+            x: '100%',
+        },
+    };
+    return (
+        <header className={cn(className, styles.header)} {...props}>
+            <Logo />
+            <ButtonIcon
+                appearance="white"
+                icon="menu"
+                onClick={() => setIsOpend(true)}
+            />
+            <motion.div
+                className={styles.mobileMenu}
+                variants={variants}
+                initial={'closed'}
+                animate={isOpend ? 'opened' : 'closed'}
+            >
+                <Sidebar />
+                <ButtonIcon
+                    className={styles.menuClose}
+                    appearance="white"
+                    icon="close"
+                    onClick={() => setIsOpend(false)}
+                />
+            </motion.div>
+        </header>
+    );
 };
