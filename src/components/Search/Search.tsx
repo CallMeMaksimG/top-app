afca0dd3ce9c535ea85fd5dffe62e6a6d7094a82
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-22 16:30:26 +0300

    Добавить landmarks

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -23,7 +23,7 @@ export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
         }
     };
     return (
-        <div className={cn(className, styles.search)} {...props}>
+        <form className={cn(className, styles.search)} {...props} role="search">
             <Input
                 className={styles.input}
                 placeholder="Поиск..."
@@ -39,6 +39,6 @@ export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
             >
                 <GlassIcon />
             </Button>
-        </div>
+        </form>
     );
 };
--- src/layout/Menu/Menu.tsx
+++ src/layout/Menu/Menu.tsx
@@ -148,5 +148,5 @@ export const Menu = (): JSX.Element => {
             </motion.div>
         ));
     };
-    return <div className={styles.menu}>{buildFirstLevel()}</div>;
+    return <nav className={styles.menu} role="navigation">{buildFirstLevel()}</nav>;
 };
