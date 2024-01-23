61738baf4814a5242f7b087f221f96b7ed4b2965
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-23 12:56:49 +0300

    Добавление яндекс метрики

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,21 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/globals.scss';
 import router from 'next/router';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 
 export default function App({ Component, pageProps }: AppProps) {
+    router.events.on('routeChangeComplete', (url: string) => {
+        if (typeof window !== 'undefined') {
+            ym('hit', url);
+        }
+    });
     return (
         <>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    // crossOrigin
-                />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link rel="preconnect" href="https://mc.yandex.ru" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&family=Noto+Sans:wght@300;400;500;700&display=swap"
                     rel="stylesheet"
@@ -23,6 +27,11 @@ export default function App({ Component, pageProps }: AppProps) {
                 />
                 <meta property="og:locale" content="ru_RU" />
             </Head>
+            <YMInitializer
+                accounts={[]}
+                options={{ webvisor: true, defer: true }}
+                version="2"
+            />
             <Component {...pageProps} />
         </>
     );
